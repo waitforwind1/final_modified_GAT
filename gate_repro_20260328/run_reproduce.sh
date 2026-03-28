d2c41eb7f99433e29c959f24a1b83f06006824a0
waitforwind1 <125422612+waitforwind1@users.noreply.github.com>
2026-03-28 18:53:46 +0800

    Update run_reproduce.sh
    补加漏掉的实验数据集

--- gate_repro_20260328/run_reproduce.sh
+++ gate_repro_20260328/run_reproduce.sh
@@ -7,7 +7,7 @@ python gate_repro_20260328/run_final_gate.py \
   --device auto \
   --runs 20 \
   --seed 0 \
-  --datasets fb_348 fb_414 fb_686 fb_698 mag_cs \
+  --datasets fb_348 fb_414 fb_686 fb_698 fb_1684 fb_1912 mag_cs mag_chem\
   --output gate_repro_20260328/log_clust_bridge_thr0515_edge_trust_20run_20260328.txt \
   --use-log-degree \
   --use-clustering-coefficient \
