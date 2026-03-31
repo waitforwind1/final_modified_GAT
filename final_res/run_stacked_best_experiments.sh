5e6804ceb751348d6f9bbfd7ed3766d86622556b
waitforwind1 <125422612+waitforwind1@users.noreply.github.com>
2026-03-31 16:24:56 +0800

    Update run_stacked_best_experiments.sh
    修改sh命令中的实验次数

--- final_res/run_stacked_best_experiments.sh
+++ final_res/run_stacked_best_experiments.sh
@@ -8,9 +8,9 @@ python final_res/run_stacked_best_experiments.py \
   --runs 20 \
   --seed 0 \
   --joint-max-attempts 6 \
-  --fb348-max-attempts 4 \
-  --fb414-max-attempts 16 \
+  --fb348-max-attempts 5 \
+  --fb414-max-attempts 10 \
   --fb698-max-attempts 6 \
   --fb1684-max-attempts 6 \
-  --magcs-max-attempts 16 \
+  --magcs-max-attempts 10 \
   --output final_res/results/stacked_best_propgraphclust_pos08_selected_20run.txt
